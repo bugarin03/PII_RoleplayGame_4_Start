4be10b930271acfd617a0f2ce1f0d0ad68b21322
Your Name <bruno.bugarin@correo.ucu.edu.uy>
2022-10-31 09:56:58 -0300

    Arreglos de clases

--- src/Library/items/MagicSword.cs
+++ src/Library/items/MagicSword.cs
@@ -2,12 +2,20 @@ namespace RoleplayGame.Items
 {
     public class MagicSword : IAttackItem
     {
+        public string Name {get;}
+        public string Description {get;}
         public int AttackPower
         {
             get
             {
                 return 20;
             }
+        }
+
+        public MagicSword()
+        {
+            this.Name = "Espada Maldita";
+            this.Description = "Espada de míticas batallas y origen desconocido";
         }
     }
 }
--- src/Library/items/PowerGauntlet.cs
+++ src/Library/items/PowerGauntlet.cs
@@ -4,6 +4,8 @@ namespace RoleplayGame.Items
 {
     public class PowerGauntlet : IAttackItem, IDefenseItem
     {
+        public string Name {get;}
+        public string Description {get;}
         public int AttackPower { get; private set; }
         public int DefensePower { get; private set; }
         public List<Gem> GemsList { get; set; }
@@ -13,6 +15,8 @@ namespace RoleplayGame.Items
             this.AttackPower = 0;
             this.DefensePower = 0;
             this.GemsList = new List<Gem>();
+            this.Name = "Guantelete de los mil poderes";
+            this.Description = "Arma digna de un dios";
         }
 
         public void AddGem(Gem gem)
